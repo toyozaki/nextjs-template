e84e8f25e9e4759e82382ad59a5655a117b2ada2
toyozaki <61676668+toyozaki@users.noreply.github.com>
2022-05-11 00:45:28 +0900

    Install styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ReactElement, ReactNode } from 'react'
 
 import { NextPage } from 'next'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 import type { AppProps } from 'next/app'
 
@@ -12,10 +13,29 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? (page => page)
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
