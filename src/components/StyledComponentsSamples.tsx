30ccba65de2b87f54b6ce1bb1571f48fd5eb75c2
toyozaki <61676668+toyozaki@users.noreply.github.com>
2022-05-11 01:15:24 +0900

    Add styled-components sample

---
+++ src/components/StyledComponentsSamples.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papaywhip;
+`
+
+export const Sample01 = () => {
+  return (
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
+  )
+}
